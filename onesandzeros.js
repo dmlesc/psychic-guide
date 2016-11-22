57e4063e19a5b31de31ee10541a3679588bbdd99
dmlesc <dmlurth@gmail.com>
2016-11-21 19:38:11 -0700

    got to print

--- onesandzeros.js
+++ onesandzeros.js
@@ -6,11 +6,24 @@ var supremeMathemathics = [
   'Wisdom',
   'Understanding',
   'Culture or Freedom',
-  'Power or Refinment',
+  'Power or Refinement',
   'Equality',
   'God',
   'Build or Destroy',
   'Born'
 ];
 
+var limit = 1111111111;
 
+for (var i = 0; i <= limit; i++) {
+  if (allonesandzeros(i.toString()))
+    console.log(i);
+}
+
+function allonesandzeros(o) {
+  for (var u = 0; u < o.length; u++) {
+    if (Number(o.charAt(u) > 1))
+      return false;
+  }
+  return true;
+}
