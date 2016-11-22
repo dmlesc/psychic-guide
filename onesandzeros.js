beb22ca8a62ff271ef09fa47c080097f0c26d887
dmlesc <dmlurth@gmail.com>
2016-11-21 18:17:15 -0700

    started onesandzeros

---
+++ onesandzeros.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var supremeMathemathics = [
+  'Cipher',
+  'Knowledge',
+  'Wisdom',
+  'Understanding',
+  'Culture or Freedom',
+  'Power or Refinment',
+  'Equality',
+  'God',
+  'Build or Destroy',
+  'Born'
+];
+
+
