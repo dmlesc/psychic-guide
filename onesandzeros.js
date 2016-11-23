2ebff3269f15dfa80935794b862e505786555569
David Lurth <dmlurth@gmail.com>
2016-11-23 14:16:26 -0700

    eyes owes and yous

--- onesandzeros.js
+++ onesandzeros.js
@@ -13,39 +13,39 @@ var supremeMath = [
   'Born'
 ];
 
-var limit = 1111111111;
+var limit = 11111;
 var teachings = [];
 var initiate = true;
 var steps = -1;
 
-function allonesandzeros(o) {
-  for (var u = 0; u < o.length; u++) {
-    if (Number(o.charAt(u) > 1))
+function allonesandzeros(O) {
+  for (var U = 0; U < O.length; U++) {
+    if (Number(O.charAt(U) > 1))
       return false;
   }
   return true;
 }
 
-function build(i) {
+function build(I) {
   var value = {};
   var definitions = [];
-  var chamber = parseInt(i, 2);
+  var chamber = parseInt(I, 2);
   value.chamber = chamber;
-  var atoms = ('' + chamber).split('');
-  for (var o = 0; o < atoms.length; o++) {
-    definitions.push(supremeMath[Number(atoms[o])]);
+  var atoms = (chamber.toString()).split('');
+  for (var O = 0; O < atoms.length; O++) {
+    definitions.push(supremeMath[Number(atoms[O])]);
   }
   value.definitions = definitions;
   return value;
 }
 
-function breakdown(i) {
+function breakdown(I) {
   var value = {};
   var ones = 0;
   var zeros = 0;
-  var cycle = ('' + i).split('');
-  for (var u = 0; u < cycle.length; u++) {
-    if (cycle[u] === '1')
+  var cycle = (I.toString()).split('');
+  for (var U = 0; U < cycle.length; U++) {
+    if (cycle[U] === '1')
       ones++;
     else
       zeros++;
@@ -54,29 +54,37 @@ function breakdown(i) {
   value.Gods = ones;
   value.Earths = zeros;
   value.Stars = bits;
-
   return value;
 }
 
-for (var i = 0; i <= limit; i++) {
+for (var I = 0; I <= limit; I++) {
   steps++;
   if (initiate)
     steps = 1;
-  if (allonesandzeros('' + i)) {
+  if (allonesandzeros(I.toString())) {
     var lesson = {};
-    lesson.zigzag = i;
+    lesson.zigzag = I;
     lesson.steps = steps;
-    lesson.build = build(i);
-    lesson.breakdown = breakdown(i);
+    lesson.build = build(I);
+    lesson.breakdown = breakdown(I);
     teachings.push(lesson);
     initiate = true;
   }
   else
     initiate = false;
 
-  if (i % 10101 === 0)
-    process.stdout.write(i + '\r');
+  if (I % 10101 === 0)
+    process.stdout.write(I + '\r');
 }
 
 for (var i = 0; i < teachings.length; i++)
   console.log(teachings[i]);
+
+/*
+{ zigzag: 1111111111,
+  steps: 1,
+  build:
+   { chamber: 1023,
+     definitions: [ 'Knowledge', 'Cipher', 'Wisdom', 'Understanding' ] },
+  breakdown: { Gods: 10, Earths: 0, Stars: 10 } }
+*/
